2704dd68f7853309654e932dda903f50a69b26f4
Renan Pansini <renanpansini15@gmail.com>
2023-04-22 19:41:00 -0400

    login, input e tela

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,20 +5,23 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPassowrdReset from './LoginPassowrdReset';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPassowrdReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPassowrdReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
